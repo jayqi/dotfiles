efbacebe788dd11ee97a72e6147ee897c04dd008
Jay Qi <jayqi@users.noreply.github.com>
2026-05-08 17:23:20 -0400

    Set terminal title to working directory

--- home/bash/.config/bash/conf.d/00-shell.bash
+++ home/bash/.config/bash/conf.d/00-shell.bash
@@ -2,3 +2,10 @@
 HISTCONTROL=ignoredups:erasedups
 HISTSIZE=10000
 HISTFILESIZE=20000
+
+# Set the terminal title to the current working directory
+set_title() {
+  local title='\w'
+  printf '\033]0;%s\007' "${title@P}"
+}
+PROMPT_COMMAND=set_title
